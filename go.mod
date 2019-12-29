707622816f1844f636265b6f1cc50ac86c5d10b9
priantikonap <priantiko@gmail.com>
2019-12-30 05:19:13 +0700

    masih error di import git

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/priantikonap/gin-gomodtry
+
+go 1.13
+
+require github.com/gin-gonic/gin v1.5.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+import "github.com/gin-gonic/gin@1.5."
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
+}
